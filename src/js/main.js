940ba313333851f273fddfdd5401ca0608db619a
David Adalberth Andersen <david@stupid-studio.com>
2014-11-30 13:32:49 +0100

    optimezed classes

--- src/js/main.js
+++ src/js/main.js
@@ -1,303 +1,287 @@
-(function(){
-	'use strict';
+(function() {
+    'use strict';
 
-	/*
-	* Tick
-	*/
+    /*
+     * Tick
+     */
 
-	function createTick(){
-		var collection = [],
-			loop = _createAnimationLoop(),
-			requestId;
+    function createTick() {
+        var collection = [],
+            loop = _createAnimationLoop(),
+            requestId;
 
-		/*
-		* Private
-		*/
+        /*
+         * Private
+         */
 
-		function _createAnimationLoop(){
-			var once = false;
+        function _createAnimationLoop() {
+            var once = false;
 
-			return function(){
-				if(once) return;		
+            return function() {
+                if (once) return;
 
-				(function _animloop(){
-					requestId = requestAnimFrame(_animloop);
-					_render();
-				})();
+                (function _animloop() {
+                    requestId = requestAnimFrame(_animloop);
+                    _render();
+                })();
 
-				once = true;
-			}
-		}
+                once = true;
+            }
+        }
 
-		function _render(){
-			for (var i = 0; i < collection.length; i++) {
-				collection[i].callback();
-			};
-		}
+        function _render() {
+            for (var i = 0; i < collection.length; i++) {
+                collection[i].callback();
+            };
+        }
 
-		function _checkCollection(){
-			if(collection.length > 0 && collection.length < 2){
-				loop();
-			}else if(collection.length === 0){
-				cancelAnimFrame(requestId);
-				loop = _createAnimationLoop();
-			}
-		}
+        function _checkCollection() {
+            if (collection.length > 0 && collection.length < 2) {
+                loop();
+            } else if (collection.length === 0) {
+                cancelAnimFrame(requestId);
+                loop = _createAnimationLoop();
+            }
+        }
 
-		/*
-		* Public
-		*/
-		return {
-			add:function(){
-				var index = collection.indexOf(arguments[0]);
-				if (index === -1) collection.push(arguments[0]);
-				_checkCollection();
-			},
-			remove:function(){
-				var index = collection.indexOf(arguments[0]);
-				if (index > -1) collection.splice(index, 1);
-				_checkCollection();
-			}
-		}
-	}
+        /*
+         * Public
+         */
+        return {
+            add: function() {
+                var index = collection.indexOf(arguments[0]);
+                if (index === -1) collection.push(arguments[0]);
+                _checkCollection();
+            },
+            remove: function() {
+                var index = collection.indexOf(arguments[0]);
+                if (index > -1) collection.splice(index, 1);
+                _checkCollection();
+            }
+        }
+    }
 
-	var tickSingleton = stupid.createSingleton(createTick);
-
-
-	/*
-	* Document Elements
-	*/
-
-	function createDocument(){
-		var $body = $('body'),
-			$window = $('window'),
-			$document = $('document');
-
-		return {
-			getBody:function(){
-				return $body;
-			},
-			getWindow:function(){
-				return $window;
-			},
-			getDocument:function(){
-				return $document;
-			},
-		}
-	}
-
-	var documentSingleton = stupid.createSingleton(createDocument);
-
-	/*
-	* Objects
-	*/
-
-	function createWithParent(){
-		var _parent = arguments[0];
-
-		return {
-			parent:_parent
-		}
-	}
+    var tickSingleton = stupid.createSingleton(createTick);
 
 
-	function createAnimationObject(){
-		var that = createWithParent(arguments[0]),
-			frameRate = 60,
-			x = Math.random() * window.innerWidth,
-			y = Math.random() * window.innerHeight,
-			animationX,
-			animationY,
-			identifier = {
-				callback: _render
-			};
-		
-		// Create Display Element
-		var $el = $('<div />').css({
-			'background-color':'black',
-			'width':'20px',
-			'height':'20px',
-			'position':'absolute',
-			'border-radius':'100%',
-			'transform':'translateX('+x+') translateY('+y+')'
-		}) 
+    /*
+     * Document Elements
+     */
 
-		documentSingleton.getInstance().getBody().append($el);
+    function createDocument() {
+        var $body = $('body'),
+            $window = $('window'),
+            $document = $('document');
 
-		/*
-		* Private
-		*/
+        return {
+            getBody: function() {
+                return $body;
+            },
+            getWindow: function() {
+                return $window;
+            },
+            getDocument: function() {
+                return $document;
+            },
+        }
+    }
 
-		function _render(){
-			_update();
-		}
+    var documentSingleton = stupid.createSingleton(createDocument);
 
-		function _startAnimation(){
-			tickSingleton.getInstance().add(identifier);
-		}
+    /*
+     * Objects
+     */
 
-		function _stopAnimation(){
-			tickSingleton.getInstance().remove(identifier);
-		}
+    function createAnimationObject() {
+        var that = { parent: arguments[0]},
+            frameRate = 60,
+            x = Math.random() * window.innerWidth,
+            y = Math.random() * window.innerHeight,
+            animationX,
+            animationY,
+            identifier = {
+                callback: _render
+            };
 
-		function _update(){
-			x = animationX();
-			y = animationY();
-			_displayPostion(x,y);			
-		}
+        // Create Display Element
+        var $el = $('<div />').css({
+            'background-color': 'black',
+            'width': '20px',
+            'height': '20px',
+            'position': 'absolute',
+            'border-radius': '100%',
+            'transform': 'translateX(' + x + ') translateY(' + y + ')'
+        })
 
-		function _displayPostion(x,y){
-			$el.css({
-				'transform':'translateX('+x+'px) translateY('+y+'px) translateZ(0px)'
-			})
-		}
+        documentSingleton.getInstance().getBody().append($el);
 
-		function _reset(){
-			_stopAnimation();
-			that.parent.touch();
-		}
+        /*
+         * Private
+         */
 
-		function _animationConstructor(easeAnimation,start,end,duration,callback){
-			var time = 0;
+        function _render() {
+            _update();
+        }
 
-			return function(){
-				if(time === duration) callback();
-				time += 1;
+        function _startAnimation() {
+            tickSingleton.getInstance().add(identifier);
+        }
 
-				return easeAnimation(time,start,end,duration);
-			}
-		}
+        function _stopAnimation() {
+            tickSingleton.getInstance().remove(identifier);
+        }
 
-		function _goto(endX,endY,time){
-			var ease = _randomEase();
-			time = parseInt(time / frameRate) || 60;
+        function _update() {
+            x = animationX();
+            y = animationY();
+            _displayPostion(x, y);
+        }
 
-			animationX = _animationConstructor(ease(), x, endX - x, time, _reset);
-			animationY = _animationConstructor(ease(), y, endY - y, time, _reset);
-			
-			_startAnimation();
-		}
+        function _displayPostion(x, y) {
+            $el.css({
+                'transform': 'translateX(' + x + 'px) translateY(' + y + 'px) translateZ(0px)'
+            })
+        }
 
-		function _randomEase(){
-			var toggle = Math.random() < 0.5 ? true : false;
-			return function (){
-				toggle = !toggle;
-				if (toggle) return Ease.easeInOutQuart;
-				return Ease.easeInOutSine;
-			}
-				
-		}
-		
-		/*
-		* Public
-		*/
+        function _reset() {
+            _stopAnimation();
+            that.parent.touch();
+        }
 
-		that.goto = _goto;
+        function _animationConstructor(easeAnimation, start, end, duration, callback) {
+            var time = 0;
 
-		that.getX = function(){
-			return x;
-		};
+            return function() {
+                if (time === duration) callback();
+                time += 1;
 
-		that.getY = function(){
-			return y;
-		};
+                return easeAnimation(time, start, end, duration);
+            }
+        }
 
-		return that;
-	}
+        function _randomEase() {
+            var toggle = Math.random() < 0.5 ? true : false;
+            return function() {
+                toggle = !toggle;
+                if (toggle) return Ease.easeInOutQuart;
+                return Ease.easeInOutSine;
+            }
+
+        }
+
+        function _goto(endX, endY, time) {
+            var randomEase = _randomEase();
+            time = parseInt(time / frameRate) || 60;
+
+            animationX = _animationConstructor(randomEase(), x, endX - x, time, _reset);
+            animationY = _animationConstructor(randomEase(), y, endY - y, time, _reset);
+
+            _startAnimation();
+        }
+
+        /*
+         * Public
+         */
+
+        that.goto = _goto;
+
+        that.getX = function() {
+            return x;
+        };
+
+        that.getY = function() {
+            return y;
+        };
+
+        return that;
+    }
 
 
-	function createElementCollection() {
-		var self,
-			ifChildrenHasTouched,
-			numberOfChildren = 20,
-			callback = {},
-			collection = [];
+    function createElementCollection() {
+        var that = {},
+            ifChildrenHasTouched,
+            callback,
+            numberOfChildren = 20,
+            collection = [];
 
-		/*
-		* Private
-		*/
-		function _createElements(){
-			for (var i = 0; i < numberOfChildren; i++) {
-				collection.push(createAnimationObject(self));
-			};
-		}
+        /*
+         * Private
+         */
+        function _createElements() {
+            for (var i = 0; i < numberOfChildren; i++) {
+                collection.push(createAnimationObject(that));
+            };
+        }
 
-		function _loopCollection(callback) {
+        function _loopCollection(callback) {
             for (var i = 0; i < collection.length; i++) {
                 callback(collection[i], i);
             };
         }
 
-        function _spreadOut(){
-        	ifChildrenHasTouched = _createCheckChildrenTouched();
+        function _spreadOut() {
+            ifChildrenHasTouched = _createCheckChildrenTouched(callback);
 
-        	_loopCollection(function(el, i){
-        		var time = 6000 + i * 500;
-
-	    		el.goto(Math.random() * window.innerWidth, Math.random() * window.innerHeight, time);
-	    		
-	    	});
+            _loopCollection(function(el, i) {
+                var time = 6000 + i * 500;
+                el.goto(Math.random() * window.innerWidth, Math.random() * window.innerHeight, time);
+            });
         }
 
-        function _createCheckChildrenTouched(){
-        	var count = 0;
-        	return function(){
-        		if(count === collection.length){
-        			setTimeout(function(){
-        				callback();
-        			},1000)
-        		}
-        		count += 1;
-        	}
+        function _createCheckChildrenTouched(func) {
+            var count = 0;
+            return function() {
+                if (count === collection.length) {
+                    func();
+                }
+                count += 1;
+            }
         }
 
-		/*
-		* Public
-		*/
-		return {
-			setSelf:function(){
-				self = arguments[0];
-			},
-			init:function(){
-				_createElements();
-				_spreadOut();
-			},
-			getSiblings:function(){
-				return collection;
-			},
-			touch:function(){
-				ifChildrenHasTouched();
-			},
-			setCallback:function(func){
-				callback = func;
-			},
-			spreadOut: _spreadOut,
-		}
-	}; 
+        /*
+         * Public
+         */
 
+        that.init = function() {
+            _createElements();
+            _spreadOut();
+        };
 
-	
+        that.getSiblings = function() {
+            return collection;
+        };
 
-	$(document).ready(function(){
+        that.touch = function() {
+            ifChildrenHasTouched();
+        };
 
-		var elementCollection = createElementCollection();
+        that.setCallback = function(func) {
+            callback = func;
+        };
 
-		elementCollection.setSelf(elementCollection);
+        that.spreadOut = _spreadOut;
 
-		elementCollection.setCallback(function(){
-			setTimeout(function(){
-				elementCollection.spreadOut();
-			},3000)
-		});
-
-		elementCollection.init();
-		elementCollection.spreadOut(); 
-
-
-	});
+        return that;
+    };
 
 
 
+
+    $(document).ready(function() {
+
+        var elementCollection = createElementCollection();
+
+        elementCollection.setCallback(function(){
+        	setTimeout(function(){
+        		elementCollection.spreadOut();
+        	},2000)
+        });
+
+        elementCollection.init();
+        elementCollection.spreadOut(); 
+
+
+    });
 
 
 
@@ -307,16 +291,4 @@
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-}())	
+}())
